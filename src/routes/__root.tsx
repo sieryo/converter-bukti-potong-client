f94f49f2ad744947a254d3d4b31252a092133592
Sieryo <rioyudanto223@gmail.com>
2025-08-29 14:00:02 +0700

    remove unused import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanstackDevtools } from "@tanstack/react-devtools";
 import { Toaster } from "@/components/ui/sonner";
 
 export const Route = createRootRoute({
